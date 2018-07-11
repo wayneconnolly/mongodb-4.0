8531a8a98cea8905ffa09a9b544d85880d21e3e6
Wayne Connolly <wayne@wayneconnolly.com>
2018-07-11 10:07:13 +1000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -m
+
+mongodb_cmd="mongod --storageEngine $STORAGE_ENGINE"
+cmd="$mongodb_cmd --bind_ip_all"
+if [ "$AUTH" == "yes" ]; then
+    cmd="$cmd --auth"
+fi
+
+if [ "$JOURNALING" == "no" ]; then
+    cmd="$cmd --nojournal"
+fi
+
+if [ "$OPLOG_SIZE" != "" ]; then
+    cmd="$cmd --oplogSize $OPLOG_SIZE"
+fi
+
+FILE=/data/db/mongodb_password_set
+$cmd & [ -e "${FILE}" ] || /set_mongodb_password.sh
+
+fg
