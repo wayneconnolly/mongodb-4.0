8b1e13ed1975d2d52bdd8e469abdf506fdd7b8ee
Wayne Connolly <wayne@wayneconnolly.com>
2018-09-19 09:01:48 +1000

    Removed debug, all tested and working

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -16,30 +16,30 @@ fi
 
 RET=1
 while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MongoDB service startup"
+    echo "\n\n=> Waiting for confirmation of MongoDB service startup\n\n"
     sleep 5
     mongo admin --eval "help" >/dev/null 2>&1
     RET=$?
 done
 
-echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
+echo "\n\n=> Creating the ${USER} user with a ${_word} password in MongoDB\n\n"
 mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
 
 if [ "$DATABASE" != "admin" ]; then
-    echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
+    echo "\n\n=> Creating the ${USER} user with a ${_word} password in MongoDB\n\n"
     mongo admin -u $USER -p $PASS << EOF
 use $DATABASE
 db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'dbOwner',db:'$DATABASE'}]})
 EOF
 fi
 
-echo "=> Done!"
+echo "\n\n=> Done!"
 touch /data/db/mongodb_password_set
-echo "$PASS" > /data/db/mongodb_password_set
+
 echo "========================================================================"
 echo "You can now connect to this MongoDB server using:"
 echo ""
 echo "    mongo $DATABASE -u $USER -p $PASS --host <host> --port <port>"
 echo ""
 echo "Please remember to change the above password as soon as possible!"
-echo "========================================================================"
+echo "========================================================================\n\n"
